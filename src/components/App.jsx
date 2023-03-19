4d0ee561685522bd0492ae9ba81abc434c39dcde
jslonina <99342408+jslonina@users.noreply.github.com>
2023-03-19 20:07:44 +0100

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { AddContacts } from './Form/Form';
+import { Section } from './Section/Section';
+import { SearchForm } from './SearchByName/SearchForm';
+import { nanoid } from 'nanoid';
+import { ContactsList } from './ContactsList/ContactsList';
+import React, { Component } from 'react';
+
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [],
+      filter: '',
+    };
+  }
+  onFormSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const name = form.elements.name;
+    const number = form.elements.number;
+
+    if (
+      this.state.contacts.find(
+        contact =>
+          contact.name === name.value && contact.number === number.value
+      )
+    ) {
+      alert(`${name.value} is already in contacts`);
+      return;
+    }
+
+    const contact = {
+      name: name.value,
+      number: number.value,
+      id: nanoid(),
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+
+    form.reset();
+  };
+
+  onFilterChange = evt => {
+    this.setState({ filter: evt.target.value });
+  };
+
+  onContactDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Section title="Phonebook">
+          <AddContacts onSubmit={this.onFormSubmit} />
+        </Section>
+        <SearchForm value={this.state.filter} onChange={this.onFilterChange} />
+        <ContactsList
+          bookArray={this.state.contacts}
+          filter={this.state.filter}
+          onDelete={this.onContactDelete}
+        />
+      </div>
+    );
+  }
+}
